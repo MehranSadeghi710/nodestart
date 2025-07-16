9124ad46c5a5353f23e8d7098adc2e829a4a69c8
MehranSadeghi710 <162831313+MehranSadeghi710@users.noreply.github.com>
2025-07-16 17:40:56 +0330

    .

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ global.config = require('./config.js');
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended : false }));
+app.set ('view engine', 'ejs');
 
+// app.use((req , res , next)=>{
+//     next();
+//     })
+
+app.use('/', require('./routes/user'));
 
 app.listen(config.port, ()=>{
     console.log(`Server started on port ${config.port}`);
